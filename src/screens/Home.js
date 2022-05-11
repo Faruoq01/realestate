f59e9d681fd538eb5466c3a61cbfca6b0e4e956c
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-11 15:06:10 +0100

    images

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../styles/Home.scss';
+import hero from '../images/hero2.jpg';
+//left: #d0601c and right #ed3535
 
 const Home = () => {
     return(
-        <div>
-            <div>Home</div>
+        <div className="container">
+            <div className="header">Home</div>
+            <div className="hero">
+                <img src={hero} alt="hero" />
+            </div>
         </div>
     )
 }

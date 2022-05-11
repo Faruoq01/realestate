84cd87119526e37591ea9ba94acc8b5ae7086254
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-11 16:58:34 +0100

    header done

---
+++ src/components/Home/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../../styles/Home.scss';
+import facebook from '../../images/facebook.png';
+import twitter from '../../images/twitter.png';
+import linkedin from '../../images/linkedin.png';
+import instagram from '../../images/instagram.png';
+import youtube from '../../images/youtube.png';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="social-row">
+                <div className="row-container">
+                    <div className="contents">
+                        <div className="social-icon">
+                            <div className="icons">
+                                <img style={{width:'25px', height:'25px'}} src={facebook} alt="facebook" />
+                                <img style={{width:'25px', height:'25px'}} src={twitter} alt="facebook" />
+                                <img style={{width:'25px', height:'25px'}} src={linkedin} alt="facebook" />
+                                <img style={{width:'25px', height:'25px'}} src={instagram} alt="facebook" />
+                                <img style={{width:'25px', height:'25px'}} src={youtube} alt="facebook" />
+                            </div>
+                        </div>
+                        <div className="logo">
+                            <div style={{fontWeight:'bolder'}}>REAL ESTATE MANAGER</div>
+                            <div style={{fontSize:'11px'}}>New real estate IT support company.</div>
+                        </div>
+                        <div className="phone-number">
+                            <div>
+                                +2347067462329
+                            </div>
+                        </div>
+                    </div>
+                    <div className="empty"></div>
+                </div>
+            </div>
+            <div className="menu-row">
+                <div className="menu-container">
+                    <div>Home</div>
+                    <div>Services</div>
+                    <div>AboutUs</div>
+                    <div>Gallery</div>
+                    <div>FAQ</div>
+                    <div>Contact</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/Home.scss';
-import hero from '../images/hero2.jpg';
-//left: #d0601c and right #ed3535
+import Header from '../components/Home/Header';
 
 const Home = () => {
     return(
         <div className="container">
-            <div className="header">Home</div>
+            <Header />
             <div className="hero">
-                <img src={hero} alt="hero" />
+                
             </div>
         </div>
     )

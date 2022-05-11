67fb8858a1867799f824780ff78bee9c544bf6e7
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-11 11:58:23 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Home from './screens/Home';
+import About from './screens/AboutUs';
+import ContactUs from './screens/ContactUs';
+import Services from './screens/Services';
+import Gallery from './screens/Gallery';
+import FAQ from './screens/FAQ';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div>Welcome to real estate!</div>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/services' component={Services}/>
+          <Route path='/contact' component={ContactUs}/>
+          <Route path='/gallery' component={Gallery}/>
+          <Route path='/faq' component={FAQ}/>
+          <Route render = {() => <h1>404 page not found</h1>} />
+        </Switch>
+      </React.Fragment>
+    </Router>
   );
 }
 

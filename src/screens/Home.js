6ff4c02520e3557ebc646ec88c635df83e2a2b95
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-12 12:31:30 +0100

    update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Home.scss';
 import Header from '../components/Home/Header';
 
 const Home = () => {
+    const options = [];
+    const [values, setValues] = useState();
     return(
         <div className="container">
+            <div className="top-level"></div>
             <Header />
             <div className="hero">
                 <div className="hero-content">
-                    <h2>Affordable Luxury & Residential Homes </h2>
-                    <div>
-                        Create React App is an officially supported 
-                        way to create single-page React applications. 
-                        It offers a modern build setup with no configuration.
-                    </div>
-                    <div className="search-container">
-                        <input type="text" placeholder={"Search"} />
-                        <button>Search</button>
+                    <div className="left-hero">first</div>
+                    <div className="right-hero">
+                        <div className="card-right">
+                            <div className="card-content">
+                                <div className="property">FIND A PROPERTY</div>
+                                <div className="select-div">
+                                    <select style={selects}>
+                                        <option>Kaduna</option>
+                                        <option>Abuja</option>
+                                        <option>Lagos</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     )
+}
+
+const selects = {
+    width:'90%',
+    height:'35px',
+    border:'none',
+    outline:'none',
+    backgroundColor:'rgba(11, 69, 69, 0)',
+    color:'#fff',
+    marginLeft:'10px'
 }
 
 export default Home;

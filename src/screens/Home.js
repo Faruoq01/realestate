dcad38cdd1d282e1d03b9fa5fb692338ff693166
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-12 18:14:52 +0100

    update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,7 +12,27 @@ const Home = () => {
             <div className="stat-section">
                 <div className="briefing">
                     <div className="breif-container">
-
+                        <div className="brief-mid">
+                            <div className="brief-title">Kaduna Metrapolis is our neighbourhood</div>
+                            <div className="paragh1">
+                                City sales was established in 1997 by prominent Kaduna real estate figure
+                                Aminu Faruk. City sales was kadunas' leading specialist agent, providing
+                                expert consultations and representations regarding apartment sales, property
+                                management and housing investment.
+                            </div>
+                            <div className="paragh1">
+                                City sales was established in 1997 by prominent Kaduna real estate figure
+                                Aminu Faruk. City sales was kadunas' leading specialist agent, providing
+                                expert consultations and representations regarding apartment sales, property
+                                management and housing investment.
+                            </div>
+                            <div className="download">
+                                DOWNLOAD THE CITY SALES APARTMENT REPORT 
+                            </div>
+                            <div className="download">
+                                DOWNLOAD THE CITY SALES APARTMENT REPORT 
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="stats">

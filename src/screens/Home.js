5cbaf8703021192044854d72fd5305aabfc80520
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-13 14:46:27 +0100

    update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,8 +2,26 @@ import React from 'react';
 import '../styles/Home.scss';
 import Header from '../components/Home/Header';
 import HeroArea from '../components/Home/HeroArea';
+import auction from '../images/auction.png';
+import delivery from '../images/delivery.png';
+import house from '../images/house.png';
+import property from '../images/property.png';
 
 const Home = () => {
+    const ServiceCard = (props) => {
+        return(
+            <div className="main-cont">
+                <div className="outer-container">
+                    <img style={{width:'50px', height:'50px'}} src={props.img} alt="icon" />
+                </div>
+                <div className="content">
+                    <div style={{fontSize:'12px', fontWeight:'bolder', color:'#0b4545'}}>{props.title}</div>
+                    <div style={{fontSize:'10px', color:'#0b4545'}}>{props.content}</div>
+                    <div style={{fontSize:'11px', fontWeight:'bolder', color:'#0b4545'}}>{props.view}</div>
+                </div>
+            </div>
+        )
+    }
     return(
         <div className="container">
             <div className="top-level"></div>
@@ -37,7 +55,30 @@ const Home = () => {
                 </div>
                 <div className="stats">
                     <div className="stats-container">
-
+                        <ServiceCard 
+                            img={auction} 
+                            title={"UPCOMING AUCTIONS"} 
+                            content={"View our upcoming auction"} 
+                            view={"VIEW AUCTIONS"}
+                        />
+                        <ServiceCard 
+                            img={house} 
+                            title={"UPCOMING AUCTIONS"} 
+                            content={"View our upcoming auction"} 
+                            view={"VIEW AUCTIONS"}
+                        />
+                        <ServiceCard 
+                            img={property} 
+                            title={"UPCOMING AUCTIONS"} 
+                            content={"View our upcoming auction"} 
+                            view={"VIEW AUCTIONS"}
+                        />
+                        <ServiceCard 
+                            img={delivery} 
+                            title={"UPCOMING AUCTIONS"} 
+                            content={"View our upcoming auction"} 
+                            view={"VIEW AUCTIONS"}
+                        />
                     </div>
                 </div>
             </div>

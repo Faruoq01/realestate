dc5e19a467532db4c5d07676991dd96c48dc30a2
Faruoq01 <faruk.aminu@mabevic.com>
2022-05-15 18:16:49 +0100

    update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
                     <div className="first">RECENT</div>
                     <div className="second">PROPERTIES</div>
                 </div>
-                <div style={{marginTop:'30px', border:'3px solid #fff'}}>
+                <div style={{marginTop:'20px', border:'3px solid #fff'}}>
                     <Carousel width={'1150px'} showThumbs={false} >
                             <div>
                                 <img style={{width:'100%', height:'500px'}} src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJlYWwlMjBlc3RhdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="icon" />
@@ -47,10 +47,56 @@ const Home = () => {
                             </div>
                     </Carousel>
                 </div>
-                <div className="estate-title">
-                    <div className="first">RECENT</div>
-                    <div className="second">PROPERTIES</div>
+                <div className="estate-title2">
+                    <div className="first2">POPULAR</div>
+                    <div className="second2">PLACES</div>
                 </div>
+                <div className="thumbnails">
+                    <div className="firstGrid">
+                        <h2>Unguwar Rimi</h2>
+                        <div>203 properties</div>
+                    </div>
+                    <div className="secondGrid">
+                        <h2>Unguwar Sarki</h2>
+                        <div>350 properties</div>
+                    </div>
+                    <div className="thirdGrid">
+                        <h2>Isa Kaita</h2>
+                        <div>130 properties</div>
+                    </div>
+                    <div className="fourthGrid">
+                        <h2>Unguwar Dosa</h2>
+                        <div>400 properties</div>
+                    </div>
+                </div>
+            </div>
+            <div className="why-us">
+
+            </div>
+            <div className= "why-details">
+                <div className="details-container">
+                    <div className="one">one</div>
+                    <div className="two">two</div>
+                    <div className="three">three</div>
+                    <div className="four">four</div>
+                </div>
+            </div>
+            <div className="agents-container">
+                <div className="estate-title">
+                    <div className="first">MEET OUR</div>
+                    <div className="second">AGENTS</div>
+                </div>
+                <div className="agent-grid">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                </div>
+            </div>
+            <div className="footer">
+
             </div>
         </div>
     )
